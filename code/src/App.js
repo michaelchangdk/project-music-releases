5dd22c90f6f7b119cf7180757a8e99efb732e7ef
Michael Chang <michaelchang@MacBook-Pro.local>
2022-03-07 21:43:28 +0100

    added carousel

--- code/src/App.js
+++ code/src/App.js
@@ -1,14 +1,26 @@
 import React from 'react'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import Cards from './components/Cards'
+// import Header from './components/Header'
+// import Footer from './components/Footer'
+// import Albums from './components/Albums'
+// import Singles from './components/Singles'
 import Playlists from './components/Playlist'
+import AlbumsSlide from './components/AlbumsSlide'
+import SinglesSlide from 'components/SinglesSlide'
+
+import data from './data.json'
+let array = data.albums.items
+console.log(array)
+
+// array.map((item) => {
+//   console.log(item.artists.length)
+// })
 
 export const App = () => {
   return (
     <section className="page">
     {/* <Header /> */}
-    <Cards />
+    <AlbumsSlide />
+    <SinglesSlide />
     <Playlists />
     {/* <Footer /> */}
     </section>
--- code/src/index.js
+++ code/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import '../node_modules/flickity/css/flickity.css'
 import './index.css'
 import { App } from './App'
 
