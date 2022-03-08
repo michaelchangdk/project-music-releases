e9aceca4f8100ff26e683d8f3c507d2cee5c28d3
Michael Chang <michaelchang@MacBook-Pro.local>
2022-03-08 12:18:54 +0100

    fixed a minor bug

--- code/src/components/SinglesSlide.jsx
+++ code/src/components/SinglesSlide.jsx
@@ -63,7 +63,7 @@ const SinglesSlide = () => {
                 } else if (numberOfArtists === 2) {
                 return (
                     <div>
-                    <a href={item.artists[0].external_urls.spotify} target="_blank" rel="noopener noreferrer"><p className="artist-name link-hover">{item.artists[0].name} &</p></a>
+                    <a href={item.artists[0].external_urls.spotify} target="_blank" rel="noopener noreferrer"><p className="artist-name link-hover">{item.artists[0].name}</p></a>
                     <span className="artist-name">&nbsp;&&nbsp;</span>
                     <a href={item.artists[1].external_urls.spotify} target="_blank" rel="noopener noreferrer"><p className="artist-name link-hover">{item.artists[1].name}</p></a>
                     </div>
