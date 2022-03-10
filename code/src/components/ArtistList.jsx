06d55cba478f664ed15cc3b7546e336262da8702
Michael Chang <michaelchang@MacBook-Pro.lan>
2022-03-10 22:25:45 +0100

    added artist list component

--- code/src/components/ArtistList.jsx
+++ code/src/components/ArtistList.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
+let numberOfArtists
 
-const ArtistList = () => {
+const ArtistList = (props) => {
+let item = props.artists
  return (
-     <div>
-     </div>
+     <>
+    {(() => {
+        numberOfArtists = item.artists.length;
+        if (numberOfArtists === 1) {
+        return (
+            <a href={item.artists[0].external_urls.spotify} target="_blank" rel="noopener noreferrer"><p className="artist-name link-hover">{item.artists[0].name}</p></a>
+        )
+        } else if (numberOfArtists === 2) {
+        return (
+            <div>
+            <a href={item.artists[0].external_urls.spotify} target="_blank" rel="noopener noreferrer"><p className="artist-name link-hover">{item.artists[0].name}</p></a>
+            <span className="artist-name">&nbsp;&&nbsp;</span>
+            <a href={item.artists[1].external_urls.spotify} target="_blank" rel="noopener noreferrer"><p className="artist-name link-hover">{item.artists[1].name}</p></a>
+            </div>
+        )
+        } else {
+        return (
+            <div>
+                <a href={item.artists[0].external_urls.spotify} target="_blank" rel="noopener noreferrer"><p className="artist-name link-hover">{item.artists[0].name}</p></a>
+                <span className="artist-name">,&nbsp;</span>
+                <a href={item.artists[1].external_urls.spotify} target="_blank" rel="noopener noreferrer"><p className="artist-name link-hover">{item.artists[1].name}</p></a>
+                <span className="artist-name">&nbsp;&&nbsp;</span>
+                <a href={item.artists[2].external_urls.spotify} target="_blank" rel="noopener noreferrer"><p className="artist-name link-hover">{item.artists[2].name}</p></a>
+            </div>
+        )
+        }
+        })()}
+        </>
  )   
 }
 
--- code/src/components/SongCard.jsx
+++ code/src/components/SongCard.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
+import ArtistList from './ArtistList'
 import heart from '../icons/heart_white.svg'
 import play from '../icons/play_white.svg'
 import dots from '../icons/dots_white.svg'
-
-let numberOfArtists
 
 const SongCard = (props) => {
     let item = props.item
@@ -21,32 +20,7 @@ const SongCard = (props) => {
                 </div>
                 <div className="card-text-group">
                     <a href={item.external_urls.spotify} rel="noopener noreferrer"><p className="album-name link-hover">{item.name}</p></a>
-                    {(() => {
-                    numberOfArtists = item.artists.length;
-                    if (numberOfArtists === 1) {
-                    return (
-                        <a href={item.artists[0].external_urls.spotify} target="_blank" rel="noopener noreferrer"><p className="artist-name link-hover">{item.artists[0].name}</p></a>
-                    )
-                    } else if (numberOfArtists === 2) {
-                    return (
-                        <div>
-                        <a href={item.artists[0].external_urls.spotify} target="_blank" rel="noopener noreferrer"><p className="artist-name link-hover">{item.artists[0].name}</p></a>
-                        <span className="artist-name">&nbsp;&&nbsp;</span>
-                        <a href={item.artists[1].external_urls.spotify} target="_blank" rel="noopener noreferrer"><p className="artist-name link-hover">{item.artists[1].name}</p></a>
-                        </div>
-                    )
-                    } else {
-                    return (
-                        <div>
-                            <a href={item.artists[0].external_urls.spotify} target="_blank" rel="noopener noreferrer"><p className="artist-name link-hover">{item.artists[0].name}</p></a>
-                            <span className="artist-name">,&nbsp;</span>
-                            <a href={item.artists[1].external_urls.spotify} target="_blank" rel="noopener noreferrer"><p className="artist-name link-hover">{item.artists[1].name}</p></a>
-                            <span className="artist-name">&nbsp;&&nbsp;</span>
-                            <a href={item.artists[2].external_urls.spotify} target="_blank" rel="noopener noreferrer"><p className="artist-name link-hover">{item.artists[2].name}</p></a>
-                        </div>
-                    )
-                    }
-                    })()}
+                    <ArtistList artists={item} />
                     </div>
                     </div>
         </>
