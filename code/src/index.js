9ecaba19cde0642ef05c00340584952463024035
Michael Chang <michaelchang@MacBook-Pro.local>
2022-03-11 20:55:46 +0100

    https://project-spotify-week9.netlify.app

--- code/src/index.js
+++ code/src/index.js
@@ -3,5 +3,6 @@ import ReactDOM from 'react-dom'
 import '../node_modules/flickity/css/flickity.css'
 import './index.css'
 import { App } from './App'
+const root = document.getElementById('root')
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, root)
